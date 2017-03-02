ff980782626e9cd11b4a4b7427a3081ac7ac5224
MacBook <MacBook@192.168.2.104>
2017-03-02 22:07:08 +0800

    添加了restful接口模式

--- SSMDemo/src/Test/Hello.java
+++ SSMDemo/src/Test/Hello.java
@@ -1,18 +1,24 @@
 package Test;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import Common.Controller.OutputStringController;
 
 @Controller
 public class Hello extends OutputStringController{
-	
-	
 	//接口形式
 	@RequestMapping(value="/Hello",produces="text/html;charset=UTF-8")
 	@ResponseBody 
 	public String sayHello(){
+		System.out.println("访问了Hello!");
 		return failure("失败");
 	}
-	
+	//restful接口
+	@RequestMapping(value = "/start/{name}", produces = "text/html;charset=UTF-8")
+	@ResponseBody 
+	public String start2(@PathVariable("name") String name1) {
+		System.out.println(name1);
+		return "start";
+	}
 }
