4c36f8d87e06d6eba00cebe748f78b703d8d3ebc
MacBook <MacBook@192.168.2.104>
2017-03-05 23:16:40 +0800

    调试成功

--- InvoiceMS/src/Common/Interceptor/UserLoginInterceptor.java
+++ InvoiceMS/src/Common/Interceptor/UserLoginInterceptor.java
@@ -2,7 +2,6 @@ package Common.Interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
