ad3fb6f55d9023d8426dfe7cf858db08b85716a2
MacBook <MacBook@192.168.0.18>
2017-04-04 23:31:49 +0800

    修正了些问题，并设计了super拦截器

--- src/Common/Interceptor/SuperUserInterceptor.java
+++ src/Common/Interceptor/SuperUserInterceptor.java
@@ -1,4 +1,5 @@
 package Common.Interceptor;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -6,40 +7,36 @@ import org.apache.log4j.Logger;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 import Common.CommonInfo;
+import Enum.UserType;
 import Model.User;
 
-/**
- * 登录过滤器
- * @author ctk
- *
- */
-public class UserLoginInterceptor implements HandlerInterceptor{
-	private static Logger logger = Logger.getLogger(UserLoginInterceptor.class);
-	//后置执行
+public class SuperUserInterceptor implements HandlerInterceptor{
+	private static Logger logger = Logger.getLogger(SuperUserInterceptor.class);
 	@Override
 	public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3)
 			throws Exception {
+		
 	}
-	//
+
 	@Override
 	public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, ModelAndView arg3)
 			throws Exception {
+		
 	}
 
-	//前置执行
 	@Override
 	public boolean preHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2) throws Exception {
-		logger.debug("用户登录拦截器的前置方法.....");
+		logger.debug("超级用户拦截器启动...");
 		HttpSession session = arg0.getSession();
 		User u = (User)session.getAttribute(CommonInfo.userInfo);
-		if(u == null)
+		if(u.getType() != UserType.SUPER.getCode())
 		{
-			logger.debug("用户未登录");
+			logger.debug("非超级用户无法访问");
 			arg1.sendRedirect("/"+CommonInfo.projName+"/index");
 			return false;
 		}
 		else
-			logger.debug("用户已登录:"+u.getUsername());
-		return true;
+			return true;
 	}
+
 }
---
+++ src/Service/InvoiceService.java
@@ -0,0 +1,8 @@
+package Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class InvoiceService {
+
+}
